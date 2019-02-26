0a434ba4b09f5f6018dd623134ea986776f847af
Chris Maillefaud <chrismaillefaud@gmail.com>
2019-02-26 11:19:05 -0300

    Rename config

--- sheep/main.py
+++ sheep/main.py
@@ -7,16 +7,15 @@ import yaml
 class Sheep:
     def __init__(self, config_file_path):
         self.config_file_path = config_file_path
-        self.sheep_config = {}
+        self.config = {}
 
     def read_config_file(self):
         file_path = Path(self.config_file_path).resolve()
         with open(file_path) as file:
-            self.sheep_config = yaml.safe_load(file)
-        return self.sheep_config
+            self.config = yaml.safe_load(file)
 
     def read_compose_file(self, project):
-        compose_file_path = self.config_sheep['compose_projects'][project]['path']
-        compose_file_list = self.config_sheep['compose_projects'][project]['files']
+        compose_file_path = self.config['compose_projects'][project]['path']
+        compose_file_list = self.config['compose_projects'][project]['files']
         for file in compose_file_list:
             file_path = Path(os.path.join(compose_file_path, file)).resolve()
--- sheep/tests.py
+++ sheep/tests.py
@@ -2,6 +2,7 @@ from sheep.main import Sheep
 
 
 def test_read_yaml_file(sheep: Sheep):
-    compose_data = sheep.read_config_file()
+    sheep.read_config_file()
+    compose_data = sheep.config
     assert isinstance(compose_data, dict)
     assert compose_data.get('compose_projects')
