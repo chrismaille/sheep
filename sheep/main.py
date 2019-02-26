f087caa03c7918c874a430a2c6699aeb675378a9
Chris Maillefaud <chrismaillefaud@gmail.com>
2019-02-26 11:27:02 -0300

    Add read compose code

--- sheep/main.py
+++ sheep/main.py
@@ -8,6 +8,7 @@ class Sheep:
     def __init__(self, config_file_path):
         self.config_file_path = config_file_path
         self.config = {}
+        self.projects = {}
 
     def read_config_file(self):
         file_path = Path(self.config_file_path).resolve()
@@ -17,5 +18,9 @@ class Sheep:
     def read_compose_file(self, project):
         compose_file_path = self.config['compose_projects'][project]['path']
         compose_file_list = self.config['compose_projects'][project]['files']
+        compose_data = {}
         for file in compose_file_list:
             file_path = Path(os.path.join(compose_file_path, file)).resolve()
+            with open(file_path) as yaml_file:
+                compose_data.update(yaml.safe_load(yaml_file))
+        self.projects[project] = compose_data
--- sheep/tests.py
+++ sheep/tests.py
@@ -6,3 +6,10 @@ def test_read_yaml_file(sheep: Sheep):
     compose_data = sheep.config
     assert isinstance(compose_data, dict)
     assert compose_data.get('compose_projects')
+
+
+def test_read_compose_data(sheep: Sheep):
+    sheep.read_config_file()
+    project = sheep.config['compose_projects'].keys()[0]
+    data = sheep.read_compose_file(project)
+    assert isinstance(data, dict)
